2d998b0d99f791c41f27413dd7618e86d1b20bfa
Rubén <ruben.hernandez58@lacetania.cat>
2023-05-09 11:30:33 +0200

    ManageUsers

---
+++ manage-users/src/App.jsx
@@ -0,0 +1,118 @@
+import React, { useEffect, useState, useRef } from 'react';
+import './App.css'
+// const obj = {obj1:4,obj2:4}
+// --> const shallowCopy = {...obj}
+// const shallowCopy = Object.assign({},obj)
+// --> const deepCopy = structuredClone(obj)
+// const arrayF = [3,4,4,4]
+// --> const copyArray = [...arrayF]
+// const copyArray = Object.assign([],arrayF)
+
+function App() {
+  const [users, setUsers] = useState(undefined);
+  const init_users = useRef(null);
+  const copy_users = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [style, setStyle] = useState(false);
+  const [change, setChange] = useState(false);
+  const [country, setCountry] = useState(false);
+
+  useEffect(() => {
+    if (!users) {
+      setIsLoading(true);
+      fetch('https://randomuser.me/api?results=20')
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        setUsers(data.results);
+        init_users.current = [...data.results];
+        copy_users.current = [...data.results];
+        setIsLoading(false);
+      });
+    }
+  }, [change]);
+
+  function sortUsers(filter1, filter2) {
+    setCountry(filter1 == "location" ? true : false);
+    setUsers(users.sort((first, second) => (first[filter1][filter2] > second[filter1][filter2]) ? 1 : -1));
+    setChange(!change);
+  }
+
+  function deleteUser(id) {
+    const result = users.filter(user => user.id.name !== id);
+    setUsers(result);
+    copy_users.current = [...result];
+    setChange(!change);
+  }
+
+  function resetUsers() {
+    setUsers([...init_users.current]);
+    copy_users.current = [...init_users.current];
+    setChange(!change);
+  }
+
+  function noOrderCountry() {
+    setUsers([...copy_users.current]);
+    setCountry(!country);
+    setChange(!change);
+  }
+
+  function filterUserCountry(event) {
+    const search = event.target.value.toLowerCase();
+    const result = search ? copy_users.current.filter(user => user.location.country.toLowerCase().includes(search)) : copy_users.current;
+    // console.log(result);
+    setUsers(result);
+    setChange(!change);
+  }
+
+  // const SortUsersName = element => users.sort((first, second) => (first.name.first > second.name.first) ? 1 : -1);
+
+  return (
+    <div className='App'>
+      <h1>Prueba técnica</h1>
+      <header>
+        <button onClick={() => setStyle((style) => !style)}>
+          Colorear filas
+        </button>
+        <button onClick={() => country ? noOrderCountry() : sortUsers("location","country")}>
+          { country ? 'No ordenar por país' : 'Ordenar por país' }
+        </button>
+        <button onClick={resetUsers}>
+          Resetear estado
+        </button>
+        <input onChange={filterUserCountry} placeholder="Filtra por país"/>
+      </header>
+      <main>
+        { isLoading || !users ? (
+          <p>Loading...</p>
+        ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Foto</th>
+              <th className='cursor' onClick={() => sortUsers("name","first")}>Nombre</th>
+              <th className='cursor' onClick={() => sortUsers("name","last")}>Apellido</th>
+              <th className='cursor' onClick={() => sortUsers("location","country")}>Pais</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+          (users ?? []).map((user,i) => (
+            <tr key={i} className={ style ? (i%2 == 0 ? 'color-1' : 'color-2') : ''}>
+              <td><img src={user.picture.large} alt='user-img' /></td>
+              <td>{user.name.first}</td>
+              <td>{user.name.last}</td>
+              <td>{user.location.country}</td>
+              <td><button onClick={() => deleteUser(user.id.name)}>Borrar</button></td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
+        )}
+      </main>
+    </div>
+  )
+}
+
+export default App
