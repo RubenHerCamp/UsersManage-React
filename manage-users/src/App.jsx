92b3f466397bbf1580eb22426f14984c1c38290a
Rubén <ruben.hernandez58@lacetania.cat>
2023-05-10 10:34:07 +0200

    ManageUsers

--- manage-users/src/App.jsx
+++ manage-users/src/App.jsx
@@ -11,11 +11,12 @@ import './App.css'
 function App() {
   const [users, setUsers] = useState(undefined);
   const init_users = useRef(null);
-  const copy_users = useRef(null);
+  // const copy_users = useRef(null);
   const [isLoading, setIsLoading] = useState(false);
   const [style, setStyle] = useState(false);
-  const [change, setChange] = useState(false);
+  // const [change, setChange] = useState(false);
   const [country, setCountry] = useState(false);
+  const [filter_users, setFilterUsers] = useState(undefined);
 
   useEffect(() => {
     if (!users) {
@@ -26,43 +27,43 @@ function App() {
         // console.log(data);
         setUsers(data.results);
         init_users.current = [...data.results];
-        copy_users.current = [...data.results];
+        // copy_users.current = [...data.results];
         setIsLoading(false);
       });
     }
-  }, [change]);
+  }, []);
 
   function sortUsers(filter1, filter2) {
     setCountry(filter1 == "location" ? true : false);
     setUsers(users.toSorted((first, second) => (first[filter1][filter2] > second[filter1][filter2]) ? 1 : -1));
-    setChange(!change);
+    // setChange(!change);
   }
 
   function deleteUser(id) {
     const result = users.filter(user => user.login.uuid !== id);
     setUsers(result);
-    copy_users.current = [...result];
-    setChange(!change);
+    setFilterUsers([...result]);
+    // setChange(!change);
   }
 
   function resetUsers() {
     setUsers([...init_users.current]);
-    copy_users.current = [...init_users.current];
-    setChange(!change);
+    setFilterUsers([...init_users.current]);
+    // setChange(!change);
   }
 
   function noOrderCountry() {
-    setUsers([...copy_users.current]);
+    // setUsers([...filter_users]);
+    setFilterUsers(undefined);
     setCountry(!country);
-    setChange(!change);
+    // setChange(!change);
   }
 
   function filterUserCountry(event) {
     const search = event.target.value.toLowerCase();
-    const result = search ? copy_users.current.filter(user => user.location.country.toLowerCase().includes(search)) : copy_users.current;
+    const result = search ? setFilterUsers(users.filter(user => user.location.country.toLowerCase().includes(search))) : setFilterUsers(undefined);
     // console.log(result);
-    setUsers(result);
-    setChange(!change);
+    // setChange(!change);
   }
 
   // const SortUsersName = element => users.sort((first, second) => (first.name.first > second.name.first) ? 1 : -1);
@@ -98,7 +99,7 @@ function App() {
           </thead>
           <tbody>
           {
-          (users ?? []).map((user,i) => (
+          (filter_users ? filter_users ?? [] : users ?? []).map((user,i) => (
             <tr key={user.login.uuid} className={ style ? (i%2 == 0 ? 'color-1' : 'color-2') : ''}>
               <td><img src={user.picture.large} alt='user-img' /></td>
               <td>{user.name.first}</td>
